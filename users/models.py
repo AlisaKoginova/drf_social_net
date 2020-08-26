3e283b7b362444af9e1c1fed707279f806417a7c
alisa <alisa.kog12@gmail.com>
2020-08-26 12:56:34 +0300

    Created user main models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.base_user import (
+    BaseUserManager, AbstractBaseUser,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users Must Have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        if not password:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(
+            email,
+            password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=40, unique=False)
+    last_name = models.CharField(max_length=40, unique=False)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
